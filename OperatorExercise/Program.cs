a78283f13b1ca9868f8ba857feda97fb29f2d04f
Stephen Sellers <stephensellers034@yahoo.com>
2024-03-26 17:01:53 -0400

    Operators-Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            AreaOfCircle(20);
+            Console.WriteLine($"The area of a circle with radius of 20 is {AreaOfCircle(20)}");
+           
+            
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+           double area = Math.PI * radius * radius;
+            return area;
         }
     }
 }
