7ffe63086f006ae6977598dd43d940e027449416
Stephen Sellers <stephensellers034@yahoo.com>
2024-03-27 11:25:35 -0400

    Operators-exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,16 +6,24 @@
         {
             int a = 17;
             int b = 4;
+
             int sum = a + b;
             int difference = a - b;
             int product = a * b;
             int quotient = a / b;
             int remainder = a % b;
-            AreaOfCircle(20);
-            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");            
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}.");
+            Console.WriteLine($"{a} - {b} is {difference}");
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} * {b} is {product}");
+            Console.WriteLine($"{a} % {b} is {remainder}");
             Console.WriteLine("What is the radius of your circle?");
-            double radius = double.Parse(Console.ReadLine());
-            Console.WriteLine($"The area of a circle with radius of 20 is {AreaOfCircle(20)}");
+
+            string userinput = Console.ReadLine();
+            double r = double.Parse(userinput);
+            AreaOfCircle(r);
+            Console.WriteLine($"The area of a circle with radius of 20 is {AreaOfCircle(r)}");
 
         }
 
